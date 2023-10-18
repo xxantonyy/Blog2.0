73044f830680da05b4ff5105ca550daf90bd8085
Anton <at0xa32@gmail.com>
2023-10-18 15:26:05 +0300

    add reducer type check

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -2,10 +2,10 @@ import { ReactNode, useEffect } from 'react';
 import { useDispatch, useStore } from 'react-redux';
 import { Reducer } from '@reduxjs/toolkit';
 // eslint-disable-next-line ttx/layer-imports
-import { StateSchemaKey, ReduxStoreWithManager } from '@/app/providers/StoreProvider/config/StateSchema';
+import { StateSchemaKey, ReduxStoreWithManager, StateSchema } from '@/app/providers/StoreProvider/config/StateSchema';
 
 export type ReducersList = {
-    [name in StateSchemaKey]?: Reducer;
+    [name in StateSchemaKey]?: Reducer<NonNullable<StateSchema[name]>>;
 }
 
 interface DynamicModuleLoaderProps {
