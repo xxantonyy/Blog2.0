f17b992420245ef7c78e4a785e5579d0d409b3a1
Anton <at0xa32@gmail.com>
2023-10-27 18:18:33 +0300

    add FeatureFlags

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,20 +1,21 @@
 /* eslint-disable max-len */
-import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { ArticleDetails } from '@/entities/Article';
+import { ArticleRating } from '@/features/articleRating';
+import { ArticleRecommendationsList } from '@/features/articleRecommendationsList';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import {
     DynamicModuleLoader,
     ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { ArticleRecommendationsList } from '@/features/articleRecommendationsList';
-import { articleDetailsCommentsReducer } from '../../model/slices/articleDetailsCommentsSlice';
-import cls from './ArticleDetailsPage.module.scss';
-import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
-import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
-import { ArticleRating } from '@/features/articleRating';
+import { getFeatureFlag } from '@/shared/lib/future';
 import { Page } from '@/widgets';
+import { articleDetailsCommentsReducer } from '../../model/slices/articleDetailsCommentsSlice';
+import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
+import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
+import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -24,6 +25,7 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
     const { t } = useTranslation();
     const { id } = useParams<{ id: string }>();
+    const isArticleRatingEnabled = getFeatureFlag('isArticleRatingEnabled');
 
     if (!id) {
         return (
@@ -38,6 +40,7 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
         articleDetailsComments: articleDetailsCommentsReducer,
     };
 
+    console.log(isArticleRatingEnabled);
     return (
         // eslint-disable-next-line i18next/no-literal-string
         <DynamicModuleLoader removeAfterUnmount reducers={reducer}>
